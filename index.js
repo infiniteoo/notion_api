8a235780847e031080c39fcf60c47baf63e89399
Troy Dorman <troydorman@gmail.com>
2021-05-21 16:57:50 -0600

    set up basic express server and created get route for to return video data to front end

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
 const getVideos = require('./services/notion');
 
-(async () => {
-    const nVideos = await getVideos();
-    console.log(nVideos);
 
-})()
+const app = express();
+
+app.get('/videos', async (req, res) => {
+    const videos = await getVideos();
+    res.json(videos);
+})
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+
 
 
 
