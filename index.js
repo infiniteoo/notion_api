dffff7ce38dda00077678b1e6da23223d3fa845d
Troy Dorman <troydorman@gmail.com>
2021-05-21 16:47:47 -0600

    successfully retreiving data from the API

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const dotenv  = require('dotenv').config();
+const { Client } = require("@notionhq/client");
 
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  })
+
+// list databases
+const listDatabases = async () => {
+
+    const rez = await notion.databases.list();
+    console.log(rez);
+
+}
+
+const getVideos = async () => {
+
+    const database_id = process.env.NOTION_DATABASE_ID
+
+    const payload = {
+        path: `databases/${database_id}/query`,
+        method: 'POST',
+      }
+
+    const { results } = await notion.request(payload)
+
+    const videos = results.map((page) => {
+        console.log(page.properties.Description.rich_text[0].text.content);
+    })
+}
+
+
+getVideos();
